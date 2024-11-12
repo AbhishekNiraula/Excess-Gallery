12e24938f8c45fe09dfcce534c7e5a6ee56e6e69
Abhishek Niraula <080bei004@ioepc.edu.np>
2024-11-13 00:32:11 +0545

    Gallery

--- api/fetch-images.js
+++ api/fetch-images.js
@@ -8,8 +8,8 @@ module.exports = async (req, res) => {
 		.expression('resource_type:image')
 		.sort_by('public_id', 'desc')
 		.max_results(150)
-		.execute()).resources;
-    res.status(200).json(response.data.resources);
+		.execute());
+    res.status(200).json(response.resources);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Failed to fetch images from Cloudinary' });
