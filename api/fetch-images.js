4602b2e21af55cbfe064eaef9ce101e381bab639
Abhishek Niraula <080bei004@ioepc.edu.np>
2024-11-13 00:56:59 +0545

    Gallery

--- api/fetch-images.js
+++ api/fetch-images.js
@@ -1,4 +1,11 @@
+require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+});
 
 module.exports = async (req, res) => {
   try {
