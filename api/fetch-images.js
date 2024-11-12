ea1e02c5e22886e698180af9213d6e3531f8cc2c
Abhishek Niraula <080bei004@ioepc.edu.np>
2024-11-13 00:39:58 +0545

    Gallery

--- api/fetch-images.js
+++ api/fetch-images.js
@@ -1,14 +1,21 @@
 const axios = require('axios');
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+	cloud_name: process.env.CLOUD_NAME,
+	api_key: process.env.API_KEY,
+	api_secret: process.env.API_SECRET,
+});
 
 module.exports = async (req, res) => {
-  const { CLOUD_NAME, API_KEY, API_SECRET } = process.env;
 
   try {
-	res = (await cloudinary.v2.search
+	const response = (await cloudinary.v2.search
 		.expression('resource_type:image')
 		.sort_by('public_id', 'desc')
 		.max_results(150)
 		.execute());
+
     res.status(200).json(response.resources);
   } catch (error) {
     console.error(error);
