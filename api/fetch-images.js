e3740123f3cd558d5447f8442b69bf75a871aa24
Abhishek Niraula <080bei004@ioepc.edu.np>
2024-11-13 10:40:18 +0545

    Gallery

--- api/fetch-images.js
+++ api/fetch-images.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
+import {Cloudinary} from "@cloudinary/url-gen";
+import {AdvancedImage} from '@cloudinary/react';
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
@@ -16,8 +18,15 @@ module.exports = async (req, res) => {
       .execute();
 
     console.log(response.resources);
+	const images = response.resources.map((image) => ({
+		...image,
+		secure_url: cloudinary.url(image.public_id, {
+		  format: 'jpg',
+		  secure: true,
+		}),
+	  }));
 
-    res.status(200).json(response.resources);
+    res.status(200).json(images);
   } catch (error) {
     console.error('Error fetching images from Cloudinary:', error);
     res.status(500).json({ error: 'Failed to fetch images from Cloudinary', details: error.message });
